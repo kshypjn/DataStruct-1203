43a95356d1467107ae8bd419d9be46c0ac6cdb2d
kshypjn <kashyapjn7@gmail.com>
2023-10-24 15:44:58 +0530

    changes

--- Heaps/buildHeaps.c
+++ Heaps/buildHeaps.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//swap function
+void swap(int* num1, int* num2) {
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
 //Build min heap
 void buildMinHeap(int* arr, int len) {
     for (int i = 1; i < len; i++) {

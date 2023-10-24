017e0fc21ce49d6472986d05b2170b9ba1cad76a
kshypjn <kashyapjn7@gmail.com>
2023-10-24 22:38:00 +0530

    avl

---
+++ midTermPrep/avlTrees.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int key;
+    struct node *left;
+    struct node *right;
+    int height;
+    
+};
+
+int getH(struct node* n){
+    if (n==NULL){
+        return 0;
+    }
+    return n->height;
+}
+
+struct node* createNode(int key){
+    struct node* node =( struct node*) malloc(sizeof(struct node));
+    node->key = key;
+    node->left = NULL;
+    node->right= NULL;
+    node->height = 1;
+    return node;
+
+}
+
+int getBalanceFactor(struct node* n){
+    if(n==NULL){
+        return 0;
+    }
+    return getH(n->left) - getH(n->right);
+}
+
